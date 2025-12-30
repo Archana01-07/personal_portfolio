afd6e53c64a7cd199e8db2aff2c97aa745bfb2bf
Archana01-07 <sr.archana_official@outlook.com>
2025-12-30 12:43:11 +0530

    all commits done

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,40 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss(),],
+// })
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+// import tailwindcss from '@tailwindcss/vite'; // ← ADD THIS
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(), // ← ADD THIS
+//   ],
+//   base: '/',
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// });
+
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Archana_Portfolio/', // ← This is the CORRECT "base" option
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  base: process.env.VITE_BASE_PATH || "/personal-portfolio",
+})
